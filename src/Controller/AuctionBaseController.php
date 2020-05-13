c2492b698ec74f71983c3a0e8a5ae679aedbd031
Tama <tamawish@gmail.com>
2020-05-13 18:04:09 +0900

    オークションコントローラー作成

---
+++ src/Controller/AuctionBaseController.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Auth\DefaultPasswordHasher; // added.
+use Cake\Event\Event; // added.
+
+class AuctionBaseController extends AppController
+{
+
+    // 初期化処理
+    public function initialize()
+    {
+        parent::initialize();
+        // 必要なコンポーネントのロード
+        $this->loadComponent('RequestHanler');
+        $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'authorize' => ['Controller'],
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'username',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginRedirect' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Users',
+                'action' => 'logout',
+            ],
+            'authError' => 'ログインしてください',
+        ]);
+    }
+
+    // ログイン処理
+    public function login() {
+        // POST時の処理
+        if($this->request->isPost()) {
+            $user = $this->Auth->identify();
+            // Authのidentifyをユーザーに設定
+            if(!empty($user)) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('ユーザー名かパスワードが間違っています。');
+        }
+    }
+
+    // ログアウト処理
+    public function logout() {
+        // セッションを破棄
+        $this->request->session()->destroy();
+        return $this->redirect($this->Auth->logout());
+    }
+
+    // 認証をおしないページの設定
+    public function isAuthorized($user = null) {
+        // 管理者はtrue
+        if($user['role'] === 'admin') {
+            return true;
+        }
+        // 一般ユーザーはAuctionControllerのみtrue, 他はfaslse
+        if($user['role'] === 'user') {
+            if($this->name == 'Auction') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        // その他はすべてfalse
+        return false;
+    }
+}
